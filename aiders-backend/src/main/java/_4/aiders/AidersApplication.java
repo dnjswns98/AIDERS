9d184329317df2774e5bb329cce49aa75aa4afd3
이창환 <ichanghwan@naver.com>
2025-07-24 12:38:24 +0900

    initial backend commit

---
+++ aiders-backend/src/main/java/_4/aiders/AidersApplication.java
@@ -0,0 +1,13 @@
+package _4.aiders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AidersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AidersApplication.class, args);
+	}
+
+}
---
+++ aiders-backend/src/test/java/_4/aiders/AidersApplicationTests.java
@@ -0,0 +1,13 @@
+package _4.aiders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AidersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
