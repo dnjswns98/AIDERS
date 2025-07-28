6269d66d9799df96bc0cef32c381986f0b0c1b8e
이창환 <ichanghwan@naver.com>
2025-07-28 12:35:54 +0900

    feat: Firestation Entity 구현

---
+++ aiders-backend/src/main/java/team1234/aiders/application/user/entity/Firestation.java
@@ -0,0 +1,24 @@
+package team1234.aiders.application.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+
+@Entity
+@DiscriminatorValue("firestation")
+@PrimaryKeyJoinColumn(name = "firestation_id")
+public class Firestation extends User {
+
+    @Column(nullable = false)
+    private Double latitude;
+
+    @Column(nullable = false)
+    private Double longitude;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String name;
+}
