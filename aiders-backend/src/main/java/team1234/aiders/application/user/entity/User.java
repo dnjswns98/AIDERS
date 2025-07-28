b12aeb7d12b71653920709864935a11df5f849e3
이창환 <ichanghwan@naver.com>
2025-07-28 11:36:48 +0900

    feat: Auditing 적용

--- aiders-backend/src/main/java/team1234/aiders/application/user/entity/User.java
+++ aiders-backend/src/main/java/team1234/aiders/application/user/entity/User.java
@@ -1,12 +1,16 @@
 package team1234.aiders.application.user.entity;
 
 import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 @DiscriminatorColumn(name = "role", discriminatorType = DiscriminatorType.STRING)
+@EntityListeners(AuditingEntityListener.class)
 public class User {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,7 +28,10 @@ public class User {
     @Column(unique = true, nullable = false)
     private String passwordResetKey;
 
+    @CreatedDate
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 
     private String accessToken;
