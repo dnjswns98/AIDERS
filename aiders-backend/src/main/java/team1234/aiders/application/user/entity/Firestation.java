30097ca223b5633fe081f0f2799039562c0d1ac7
이창환 <ichanghwan@naver.com>
2025-07-28 13:13:14 +0900

    fix: firestation entity 수정 lombok 추가

--- aiders-backend/src/main/java/team1234/aiders/application/user/entity/Firestation.java
+++ aiders-backend/src/main/java/team1234/aiders/application/user/entity/Firestation.java
@@ -4,10 +4,15 @@ import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import jakarta.persistence.PrimaryKeyJoinColumn;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @DiscriminatorValue("firestation")
 @PrimaryKeyJoinColumn(name = "firestation_id")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Firestation extends User {
 
     @Column(nullable = false)
