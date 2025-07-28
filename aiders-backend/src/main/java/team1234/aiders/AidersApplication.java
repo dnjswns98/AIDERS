53173cda6d3c15d70d4211f96f85bf5d15e3e526
이창환 <ichanghwan@naver.com>
2025-07-28 11:31:04 +0900

    fea: JPA config 구현

--- aiders-backend/src/main/java/team1234/aiders/AidersApplication.java
+++ aiders-backend/src/main/java/team1234/aiders/AidersApplication.java
@@ -2,8 +2,10 @@ package team1234.aiders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AidersApplication {
 
 	public static void main(String[] args) {
