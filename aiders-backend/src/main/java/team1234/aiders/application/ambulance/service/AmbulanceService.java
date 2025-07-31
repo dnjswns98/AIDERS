2377874f04ab5b171dcceffa1ae42ba0be4ac177
이창환 <ichanghwan@naver.com>
2025-07-31 09:42:15 +0900

    refactor: 메서드 추출 및 중복 제거

--- aiders-backend/src/main/java/team1234/aiders/application/ambulance/service/AmbulanceService.java
+++ aiders-backend/src/main/java/team1234/aiders/application/ambulance/service/AmbulanceService.java
@@ -3,6 +3,8 @@ package team1234.aiders.application.ambulance.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import team1234.aiders.application.ambulance.dto.PatientOptionalInfoRequestDto;
+import team1234.aiders.application.ambulance.dto.PatientRequiredInfoRequestDto;
 import team1234.aiders.application.ambulance.entity.AmbCurrentStatus;
 import team1234.aiders.application.ambulance.entity.Ambulance;
 import team1234.aiders.application.ambulance.repository.AmbulanceRepository;
@@ -10,20 +12,23 @@ import team1234.aiders.config.security.CustomUserDetails;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class AmbulanceService {
 
     private final AmbulanceRepository ambulanceRepository;
 
-    @Transactional
     public void transferToWait(CustomUserDetails user) {
-        Long userId = user.getId();
-
-        Ambulance ambulance = ambulanceRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+        Ambulance ambulance = findAmbulanceUser(user);
 
         ambulance.changeStatus(AmbCurrentStatus.WAIT);
         ambulance.clearPatientInfo();
         ambulance.clearHospitalInfo();
-        ambulance.clearHospitalInfo();
+    }
+
+    private Ambulance findAmbulanceUser(CustomUserDetails user) {
+        Long ambulanceId = user.getId();
+
+        return ambulanceRepository.findById(ambulanceId)
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
     }
 }
