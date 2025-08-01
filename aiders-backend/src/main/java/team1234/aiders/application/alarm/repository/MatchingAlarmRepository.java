9fc3050e8787acb4820254ea72830e4a8c7baf40
박원준 <tngkrt2004@gmail.com>
2025-08-01 17:02:16 +0900

    feat: MatchingAlarmRepository 추가

---
+++ aiders-backend/src/main/java/team1234/aiders/application/alarm/repository/MatchingAlarmRepository.java
@@ -0,0 +1,7 @@
+package team1234.aiders.application.alarm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import team1234.aiders.application.alarm.entity.MatchingAlarm;
+
+public interface MatchingAlarmRepository extends JpaRepository<MatchingAlarm, Long> {
+}
