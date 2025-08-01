f9bd3f77dee32ba2f928c88a52a7afd1e4401b8b
박원준 <tngkrt2004@gmail.com>
2025-08-01 17:03:36 +0900

    feat: RequestAlarmRepository 추가

---
+++ aiders-backend/src/main/java/team1234/aiders/application/alarm/repository/RequestAlarmRepository.java
@@ -0,0 +1,6 @@
+package team1234.aiders.application.alarm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RequestAlarmRepository extends JpaRepository<RequestAlarmRepository, Long> {
+}
