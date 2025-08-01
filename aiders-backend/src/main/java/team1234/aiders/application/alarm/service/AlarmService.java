0c8aa5e110740ed7b7d739bc735296d77f991328
박원준 <tngkrt2004@gmail.com>
2025-08-01 16:59:12 +0900

    feat: db에 알람 저장 임시 구현
    추후 역할 분리 구현

--- aiders-backend/src/main/java/team1234/aiders/application/alarm/service/AlarmService.java
+++ aiders-backend/src/main/java/team1234/aiders/application/alarm/service/AlarmService.java
@@ -3,6 +3,7 @@ package team1234.aiders.application.alarm.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import team1234.aiders.application.alarm.dto.AlarmMessage;
+import team1234.aiders.application.ambulance.entity.Ambulance;
 import team1234.aiders.application.ambulance.repository.AmbulanceRepository;
 
 @Service
@@ -12,6 +13,8 @@ public class AlarmService {
     private final AmbulanceRepository ambulanceRepository;
 
     public void saveAlarm(AlarmMessage message) {
+        Ambulance ambulance = ambulanceRepository.findByUserKey(message.getAmbulanceKey())
+                .orElseThrow(() -> new IllegalArgumentException("해당 구급차를 찾을 수 없습니다."));
         // message.getType()에 따라 서로 다른 알람 저장
         switch (message.getType()) {
             case MATCHING -> {
