6c4a2db36a6d1dbd5ea6c822d0692f96e950434c
박원준 <tngkrt2004@gmail.com>
2025-08-02 22:14:21 +0900

    refactor: extends JpaRepository 수정

--- aiders-backend/src/main/java/team1234/aiders/application/alarm/repository/RequestAlarmRepository.java
+++ aiders-backend/src/main/java/team1234/aiders/application/alarm/repository/RequestAlarmRepository.java
@@ -1,6 +1,7 @@
 package team1234.aiders.application.alarm.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import team1234.aiders.application.alarm.entity.RequestAlarm;
 
-public interface RequestAlarmRepository extends JpaRepository<RequestAlarmRepository, Long> {
+public interface RequestAlarmRepository extends JpaRepository<RequestAlarm, Long> {
 }
