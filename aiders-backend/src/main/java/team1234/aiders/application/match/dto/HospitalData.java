ad7295da90828ac7ea7216464182c85aa4e33651
박원준 <tngkrt2004@gmail.com>
2025-08-04 23:32:57 +0900

    feat: HospitalData dto 추가

---
+++ aiders-backend/src/main/java/team1234/aiders/application/match/dto/HospitalData.java
@@ -0,0 +1,17 @@
+package team1234.aiders.application.match.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import team1234.aiders.application.hospital.entity.Hospital;
+import team1234.aiders.application.hospital.entity.HospitalDepartment;
+import team1234.aiders.application.hospital.entity.EmergencyBed;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HospitalData {
+    private Hospital hospital;
+    private HospitalDepartment department;
+    private EmergencyBed bed;
+}
