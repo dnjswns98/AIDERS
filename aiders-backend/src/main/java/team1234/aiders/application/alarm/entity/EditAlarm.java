18e537eb7246235e29a86ae7545a55c82b0d8dec
이창환 <ichanghwan@naver.com>
2025-08-04 13:11:37 +0900

    fix: alarm 생성 시간 추가

--- aiders-backend/src/main/java/team1234/aiders/application/alarm/entity/EditAlarm.java
+++ aiders-backend/src/main/java/team1234/aiders/application/alarm/entity/EditAlarm.java
@@ -2,13 +2,18 @@ package team1234.aiders.application.alarm.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import team1234.aiders.application.ambulance.entity.Ambulance;
 import team1234.aiders.application.hospital.entity.Hospital;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
+@EntityListeners(AuditingEntityListener.class)
 @AllArgsConstructor
 public class EditAlarm {
 
@@ -24,4 +29,8 @@ public class EditAlarm {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "hospital_id", nullable = false)
     private Hospital hospital;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
 }
