86dc9f23a3bf5556c3553a521ed7750446433ef2
박원준 <tngkrt2004@gmail.com>
2025-08-05 03:30:34 +0900

    feat: HospitalData에 QueryDSL용 생성자(@QueryProjection) 추가

--- aiders-backend/src/main/java/team1234/aiders/application/hospital/dto/HospitalData.java
+++ aiders-backend/src/main/java/team1234/aiders/application/hospital/dto/HospitalData.java
@@ -1,5 +1,6 @@
 package team1234.aiders.application.hospital.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +10,16 @@ import team1234.aiders.application.hospital.entity.EmergencyBed;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class HospitalData {
     private Hospital hospital;
     private HospitalDepartment department;
     private EmergencyBed bed;
+
+    @QueryProjection
+    public HospitalData(Hospital hospital, HospitalDepartment department, EmergencyBed bed) {
+        this.hospital = hospital;
+        this.department = department;
+        this.bed = bed;
+    }
 }
+
