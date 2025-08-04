ee93bc96fa4f09be9a5ac304b6f6743c5840fd92
박원준 <tngkrt2004@gmail.com>
2025-08-05 00:48:45 +0900

    feat: hospital, dept, bed 조인 조회

--- aiders-backend/src/main/java/team1234/aiders/application/hospital/repository/HospitalRepository.java
+++ aiders-backend/src/main/java/team1234/aiders/application/hospital/repository/HospitalRepository.java
@@ -1,7 +1,17 @@
 package team1234.aiders.application.hospital.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import team1234.aiders.application.hospital.entity.Hospital;
+import team1234.aiders.application.hospital.dto.HospitalData;
+
+import java.util.List;
 
 public interface HospitalRepository extends JpaRepository<Hospital, Long> {
+
+    @Query("""
+        SELECT new team1234.aiders.application.hospital.dto.HospitalData(h, h.department, h.bed)
+        FROM Hospital h
+    """)
+    List<HospitalData> findHospitalsWithDeptAndBeds();
 }
