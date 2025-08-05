b0a0d53990ab446a8fa3f367d9cf2447215da84c
박원준 <tngkrt2004@gmail.com>
2025-08-05 12:51:54 +0900

    feat: MatchResponse dto 추가

---
+++ aiders-backend/src/main/java/team1234/aiders/application/match/dto/MatchResponse.java
@@ -0,0 +1,12 @@
+package team1234.aiders.application.match.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MatchResponse {
+    private Long hospitalId;
+    private String name;
+    private String address;
+}
