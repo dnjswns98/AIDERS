cf96ba91c238791e03c2e28934902cca4eaae20d
이창환 <ichanghwan@naver.com>
2025-08-07 09:46:19 +0900

    feat: 소속 소방서 구급차 조회

--- aiders-backend/src/main/java/team1234/aiders/application/ambulance/controller/AmbulanceController.java
+++ aiders-backend/src/main/java/team1234/aiders/application/ambulance/controller/AmbulanceController.java
@@ -1,13 +1,18 @@
 package team1234.aiders.application.ambulance.controller;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import team1234.aiders.application.ambulance.dto.AmbulanceResponseDto;
 import team1234.aiders.application.ambulance.service.AmbulanceService;
 import team1234.aiders.config.security.CustomUserDetails;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,6 +20,12 @@ import team1234.aiders.config.security.CustomUserDetails;
 public class AmbulanceController {
 
     private AmbulanceService ambulanceService;
+
+    @GetMapping("/list")
+    public ResponseEntity<List<AmbulanceResponseDto>> getAllAmbulances(@AuthenticationPrincipal CustomUserDetails user) {
+        List<AmbulanceResponseDto> result = ambulanceService.getAmbulances(user);
+        return ResponseEntity.ok(result);
+    }
 
     @PostMapping("/transfer/wait")
     public ResponseEntity<Void> transferToWait(@AuthenticationPrincipal CustomUserDetails user) {
