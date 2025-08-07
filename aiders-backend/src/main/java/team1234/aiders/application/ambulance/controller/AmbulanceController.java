e7ff672a3b07bed4dfe5441a8fbf57c8328daffb
이창환 <ichanghwan@naver.com>
2025-08-07 09:49:59 +0900

    fix: Spring이 의존성 주입

--- aiders-backend/src/main/java/team1234/aiders/application/ambulance/controller/AmbulanceController.java
+++ aiders-backend/src/main/java/team1234/aiders/application/ambulance/controller/AmbulanceController.java
@@ -19,7 +19,7 @@ import java.util.List;
 @RequestMapping("/api/v1/ambulance")
 public class AmbulanceController {
 
-    private AmbulanceService ambulanceService;
+    private final AmbulanceService ambulanceService;
 
     @GetMapping("/list")
     public ResponseEntity<List<AmbulanceResponseDto>> getAllAmbulances(@AuthenticationPrincipal CustomUserDetails user) {
