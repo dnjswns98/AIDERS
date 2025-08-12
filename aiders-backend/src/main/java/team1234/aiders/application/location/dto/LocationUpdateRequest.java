617b6249ce953552508f32202d67fc7bc960f406
박원준 <tngkrt2004@gmail.com>
2025-08-12 13:41:31 +0900

    feat: 실시간 거리 계산 dto에 구급차번호(ambulanceNumber) 추가

--- aiders-backend/src/main/java/team1234/aiders/application/location/dto/LocationUpdateRequest.java
+++ aiders-backend/src/main/java/team1234/aiders/application/location/dto/LocationUpdateRequest.java
@@ -2,6 +2,7 @@ package team1234.aiders.application.location.dto;
 
 public record LocationUpdateRequest(
         Long ambulanceId,
+        String ambulanceNumber,
         double latitude,
         double longitude
 ) {}
