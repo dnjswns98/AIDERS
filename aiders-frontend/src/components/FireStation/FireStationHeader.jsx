8f1117691809fd2785ec6945fa83176f830bd1a7
이창환 <ichanghwan@naver.com>
2025-08-15 02:42:47 +0900

    style: 소방서 헤더 아이콘 삽입

--- aiders-frontend/src/components/FireStation/FireStationHeader.jsx
+++ aiders-frontend/src/components/FireStation/FireStationHeader.jsx
@@ -41,6 +41,8 @@ const FireStationHeader = () => {
                 }`
               }
             >
+              <i className="fas fa-file-invoice mr-2 text-white"></i>
+
               보고서
             </NavLink>
           </nav>
