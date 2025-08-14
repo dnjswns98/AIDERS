73e7eeabb167c4baef6603770bdc328679213e60
박원준 <tngkrt2004@gmail.com>
2025-08-14 16:19:30 +0900

    feat: 이송완료 알람 추가

--- aiders-backend/src/main/java/team1234/aiders/application/alarm/dto/AlarmType.java
+++ aiders-backend/src/main/java/team1234/aiders/application/alarm/dto/AlarmType.java
@@ -3,5 +3,6 @@ package team1234.aiders.application.alarm.dto;
 public enum AlarmType {
     MATCHING,    // 매칭 완료 알림
     REQUEST,     // 통화 요청 알림
-    EDIT         // 환자 정보 수정 알림
+    EDIT,         // 환자 정보 수정 알림
+    COMPLETE
 }
